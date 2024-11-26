23f6520763a16f72569123f175a5cc240ed8ea79
joamksh <joamksh@naver.com>
2024-11-26 20:56:16 +0900

    feat : 값 제한, 코드 refactoring

--- src/main/java/org/example/spring/dto/MemberRequestDTO.java
+++ src/main/java/org/example/spring/dto/MemberRequestDTO.java
@@ -1,6 +1,10 @@
 package org.example.spring.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.example.spring.validation.annotation.ExistCategories;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -8,13 +12,21 @@ public class MemberRequestDTO {
 
     @Getter
     public static class JoinDto{
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
+        @ExistCategories
         List<Long> preferCategory;
         String email;
     }
